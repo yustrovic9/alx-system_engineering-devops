5fb51d5350d6c84655b963597dbf7dfea60d113c
yustrovic9 <yustrovic09@gmail.com>
2022-10-14 11:15:06 -0700

    commit 0-create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# creates a file in the /tmp directory
-file { '/tmp/holberton':
-  ensure  => 'file',
+# Create a file in /tmp
+file {'/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
